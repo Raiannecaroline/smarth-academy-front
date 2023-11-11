14b857d9a6f96dc59a342f5dada2aafe2251953d
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2023-11-11 04:32:30 -0300

    Update: Outra requisição pronta

--- src/app/components/fluxo-pessoas/fluxo-pessoas.component.ts
+++ src/app/components/fluxo-pessoas/fluxo-pessoas.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FluxoPessoas } from 'src/app/models/fluxo-pessoas.models';
+import { FluxoPessoasService } from 'src/app/service/fluxo-pessoas.service';
+// import { FluxoPessoas } from 'src/app/models/fluxo-pessoas.models';
 
 @Component({
   selector: 'app-fluxo-pessoas',
   templateUrl: './fluxo-pessoas.component.html',
   styleUrls: ['./fluxo-pessoas.component.css']
 })
+
 export class FluxoPessoasComponent implements OnInit {
 
-  constructor() { }
+  fluxo: FluxoPessoas | undefined;
+
+  constructor(
+    private fluxoService: FluxoPessoasService,
+  ) { }
 
   ngOnInit(): void {
+    this.getFluxoPessoas();
+  }
+
+  getFluxoPessoas(): void {
+    this.fluxoService.getFluxoPessoasId()
+    .subscribe(
+      data => {
+        this.fluxo = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
   }
 
 }
--- src/app/service/fluxo-pessoas.service.ts
+++ src/app/service/fluxo-pessoas.service.ts
@@ -1,9 +1,23 @@
+import { FluxoPessoas } from 'src/app/models/fluxo-pessoas.models';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+// import { FluxoPessoas } from '../models/fluxo-pessoas.models';
+
+const baseUrl = 'http://icaroaurich.dyndns.org:8022/fluxo'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FluxoPessoasService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+  getFluxoPessoasId(): Observable<FluxoPessoas> {
+    return this.http.get<FluxoPessoas>(`${baseUrl}/`);
+  }
+
 }
