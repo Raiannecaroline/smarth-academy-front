6900206b42afc3a03b8efa310274db33bf8a28cf
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2023-11-11 06:38:06 -0300

    Criação de uma novo componente

--- src/app/components/detalhes-especialistas/detalhes-especialistas.component.ts
+++ src/app/components/detalhes-especialistas/detalhes-especialistas.component.ts
@@ -1,15 +1,35 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-detalhes-especialistas',
   templateUrl: './detalhes-especialistas.component.html',
-  styleUrls: ['./detalhes-especialistas.component.css']
+  styleUrls: ['./detalhes-especialistas.component.css'],
+  animations: [
+    trigger('searchBar', [
+      state('closed', style({
+        width: '0',
+        opacity: '0',
+      })),
+      state('open', style({
+        width: '100%',
+        opacity: '1',
+      })),
+      transition('closed <=> open', animate('300ms ease-in-out')),
+    ]),
+  ],
 })
 export class DetalhesEspecialistasComponent implements OnInit {
+
+  searchOpen = 'closed';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleSearch() {
+    this.searchOpen = this.searchOpen === 'closed' ? 'open' : 'closed';
   }
 
 }
